a7cffe8e234dd799f8041fbc6871c9887b8f3427
Joanna Pokora <153290431+JoannaPokora@users.noreply.github.com>
2024-05-18 20:00:49 +0200

    Update prec_matr_est.R

--- prec_matr_est.R
+++ prec_matr_est.R
@@ -1,38 +1,53 @@
 estimate_precision_matrix <- function(dat, penalty, treshold){
   # Estimate covariance matrix
-  est.cov.matr <- apply(dat, 2, function(variable) variable-mean(variable))
+  sample.mean <- apply(dat, 2, mean)
   est.cov.matr <- 1/nrow(dat) *
-    Reduce('+', apply(est.cov.matr, 1, function(obs.vec){
-      obs.vec %*% t(obs.vec)
+    Reduce('+', apply(dat, 1, function(sample){
+      scaled.sample <- sample - sample.mean
+      scaled.sample %*% t(scaled.sample)
     }, simplify = FALSE))
   
-  # Calculate penaltized estimated covariance matrix
-  est.cov.pen.matr <- est.cov.matr + diag(penalty, nrow = ncol(dat))
+  # Calculate inverse precision matrix
+  inv.prec.matr <- est.cov.matr + diag(penalty, nrow = ncol(dat))
+  inv.prec.matr.old <- inv.prec.matr
   
   # Initialize beta estimator
-  beta.hat <- list(old = 0, new = 0)
-  # Graphical LASSO
-  while(any(abs(beta.hat[["new"]]-beta.hat[["old"]]) > treshold) |
-        all(beta.hat[["new"]] == 0)){
+  beta.hat <- list(old = rep(0, ncol(dat)),
+                   new = rep(0, ncol(dat)))
+  
+  # Perform lasso algorithm
+  while(any(abs(as.vector(inv.prec.matr-inv.prec.matr.old)) > treshold) |
+        all(as.vector(inv.prec.matr == inv.prec.matr.old))){
+    inv.prec.matr.old <- inv.prec.matr
     sapply(1:ncol(dat), function(i){
-      dif <- est.cov.matr[-i,i]- sapply(1:ncol(dat)-1, function(j){
-        sum(est.cov.pen.matr[-i,-i][-j,j]*beta.hat[["new"]][i])
-      })
-      soft.tresh.op <- sign(dif)*pmax(abs(dif)-penalty, 0)
-      beta.hat[["old"]] <<- beta.hat[["new"]]
-      beta.hat[["new"]] <<- soft.tresh.op/diag(est.cov.pen.matr[-i,-i])
+      crop.beta.hat <- beta.hat[["new"]][-i]
+      crop.est.cov.matr <- est.cov.matr[-i,i]
+      crop.inv.prec.matr <- inv.prec.matr[-i,-i]
+      while(any(abs(crop.beta.hat-beta.hat[["old"]][-i]) > treshold) |
+            all(beta.hat[["new"]] == beta.hat[["old"]])){
+        print(abs(crop.beta.hat-beta.hat[["old"]][-i]))
+        beta.hat[["old"]][-i] <<- crop.beta.hat
+        sapply(1:(ncol(dat)-1), function(j){
+          dif <- crop.est.cov.matr[j]-
+            sum(crop.inv.prec.matr[-j,j]*crop.beta.hat[-j])
+          soft.tresh.op <- sign(dif)*max(abs(dif)-penalty,0)
+          crop.beta.hat[j] <<- soft.tresh.op/crop.inv.prec.matr[j,j]
+        })
+      }
+      beta.hat[["new"]][-i] <<- crop.beta.hat
       
-      # Update penaltizied covariance matrix
-      est.cov.pen.matr[-i,i] <<- est.cov.pen.matr[-i,-i] %*%
+      # Update inverse precision matrix
+      inv.prec.matr[-i,i] <<- inv.prec.matr[-i,-i] %*%
         beta.hat[["new"]][-i]
     })
   }
   
-  prec.matr <- diag(est.cov.pen.matr)
+  # Calculate precision matrix
+  prec.matr <- matrix(0, nrow = ncol(dat), ncol = ncol(dat))
   sapply(1:ncol(dat), function(i){
-    prec.matr[-i,i] <<- -beta.hat[["new"]][-i]*
-      solve(est.cov.pen.matr[i,i]-
-              t(est.cov.pen.matr[-i,i])%*%beta.hat[["new"]][-i])
+    prec.matr[i,i] <<- solve(inv.prec.matr[i,i]-
+                               t(inv.prec.matr[-i,i])%*%beta.hat[["new"]][-i])
+    prec.matr[-i,i] <<- -beta.hat[["new"]][-i]*prec.matr[i,i]
   })
   
   prec.matr
@@ -40,6 +55,4 @@ estimate_precision_matrix <- function(dat, penalty, treshold){
 
 dat <- matrix(c(4,5,3,8,2,1,5,6,3,9,4,2), ncol = 3)
 
-estimate_precision_matrix(dat, 0, 0.00001)
-
-          
+estimate_precision_matrix(dat, 0, 0.001)
